88a838654093640c7927077ad9832a1187c2106d
HandyWang <baweier2015@sina.com>
2016-11-22 22:40:31 +0800

    后台管理首页添加

--- MyMusicWorld/mmw.Web/Controllers/HomeController.cs
+++ MyMusicWorld/mmw.Web/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,6 +16,22 @@ namespace mmw.Web.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            var songs = context.Songs.ToList();
+            StringBuilder songStr = new StringBuilder();
+
+            foreach (var item in songs)
+            {
+                songStr.AppendFormat("\"test\":{0}",item.SongTitle);
+                songStr.AppendFormat("{\"mp3\":\"{0}\"", item.Path);
+                songStr.AppendFormat("\"title\":\"{0}\"}", item.SongTitle);
+
+
+                songStr.AppendFormat("{\"mp3\":\"{0}\",\"title\":\"{1}\",\"artist\":\"{2}\"},", item.Path, item.SongTitle, item.Singer.SingerName);
+            }
+
+            songStr.Remove(songStr.Length - 1, 1).Insert(0, '[').Append(']');
+            ViewBag.songs = songStr;
+
             return View();
         }
     }
