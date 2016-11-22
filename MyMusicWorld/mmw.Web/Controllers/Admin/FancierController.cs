583f1dbb3608d27f3a10c8e651161d1f71191a0c
WangHongyuan <wanghongyuan@yufu.cn>
2016-11-22 11:34:57 +0800

    后台管理完善_歌手，发烧友

---
+++ MyMusicWorld/mmw.Web/Controllers/Admin/FancierController.cs
@@ -0,0 +1,49 @@
+﻿using mmw.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mmw.Web.Controllers.Admin
+{
+    public class FancierController : BaseController
+    {
+        MusicDbContext context = CurContext();
+
+        // GET: Fancier
+        public ActionResult Index()
+        {
+            var fanciers = (from f in context.Fanciers
+                            select f).ToList();
+            var grades = (from g in context.Grades
+                          select g).ToList();
+            ViewBag.grades = grades;
+            return View(fanciers);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var fancier = context.Fanciers.Find(id);
+            if (fancier!=null)
+            {
+                context.Entry(fancier).State = EntityState.Deleted;
+                context.Fanciers.Remove(fancier);
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Add(Fancier fancier)
+        {
+            fancier.JoinTime = DateTime.Now;
+            context.Fanciers.Add(fancier);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MyMusicWorld/mmw.Web/Controllers/Admin/SingerController.cs
@@ -0,0 +1,50 @@
+﻿using mmw.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mmw.Web.Controllers.Admin
+{
+    public class SingerController : BaseController
+    {
+        //当前上下文
+        MusicDbContext context = CurContext();
+
+        // GET: Singer
+        public ActionResult Index()
+        {
+            var areas = context.Areas.ToList();
+            var singers = context.Singers.ToList();
+            ViewBag.Areas = areas;
+            return View(singers);
+        }
+
+        [HttpPost]
+        public ActionResult Add(Singer singer)
+        {
+            context.Singers.Add(singer);
+            var result = context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// 删除歌手信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Delete(int id)
+        {
+            var singer = context.Singers.Find(id);
+            if (singer!=null)
+            {
+                context.Entry(singer).State = EntityState.Deleted;
+                context.Singers.Remove(singer);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
