38dae6ff4db5d07fb80ded9158b2d104fc954bec
HandyWang <baweier2015@sina.com>
2016-12-06 21:05:22 +0800

    样式完善

--- MyMusicWorld/mmw.Web/Controllers/HomeController.cs
+++ MyMusicWorld/mmw.Web/Controllers/HomeController.cs
@@ -18,20 +18,15 @@ namespace mmw.Web.Controllers
         {
             var songs = context.Songs.ToList();
             StringBuilder songStr = new StringBuilder();
-
             foreach (var item in songs)
             {
-                songStr.AppendFormat("\"test\":{0}",item.SongTitle);
-                songStr.AppendFormat("{\"mp3\":\"{0}\"", item.Path);
-                songStr.AppendFormat("\"title\":\"{0}\"}", item.SongTitle);
-
-
-                songStr.AppendFormat("{\"mp3\":\"{0}\",\"title\":\"{1}\",\"artist\":\"{2}\"},", item.Path, item.SongTitle, item.Singer.SingerName);
+                songStr.Append("{");
+                songStr.AppendFormat("\"mp3\":\"{0}\",\"title\":\"{1}\",\"artist\":\"{2}\",\"duration\":\"3:50 \"", item.Path, item.SongTitle, item.Singer.SingerName);
+                songStr.Append("},");
             }
 
             songStr.Remove(songStr.Length - 1, 1).Insert(0, '[').Append(']');
-            ViewBag.songs = songStr;
-
+            ViewBag.songs = songStr.ToString();
             return View();
         }
     }
